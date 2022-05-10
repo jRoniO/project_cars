a3d001c2d11e1bb6cbbb8f7b798623596aae3996
fakhreddin98 <99963603+fakhreddin98@users.noreply.github.com>
2022-05-11 00:03:11 +0200

    Update header.h

--- Fakhreddin/header.h
+++ Fakhreddin/header.h
@@ -1,144 +1,54 @@
-#include "header.h"
+// Include the servo library:
+#include <Servo.h>
+#include <OPT3101.h>
+#include <Wire.h>
 
 
-void setup() 
+// Create a new servo object:
+Servo myservo;
+
+// Define the servo pin:
+#define servoPin 5
+
+const int IN1 = 7; // Pin nummer för motor 
+const int IN2 = 4; // Pin nummer för motor 
+const int ENA = 3; // Pin nummer för motors riktning
+
+const uint8_t dataReadyPin = 2; // Pin nummer för sensor läsning
+
+int angle = 90; // Döper svängnings vinkel till angle
+int Speed = 100; // Döper hasigheten till Speed
+
+int leftsensor = 0;
+int middlesensor = 1;
+int rightsensor = 2;
+
+// Namn för olika hasigheter för lättare ändring av hastigheten.
+int BackSpeed = 160;
+int LowSpeed = 170;
+int MiddleSpeed = 205;
+int HighSpeed = 255;
+
+// Namn för olika sväng vinklar för lättare ändring av svängvinkel
+int TurnRight = 135; // Döper HögerSvängnings vinkel
+int TurnLeft = 45; // Döper VänsterSvängnings vinkel 
+int Straight = 90; // Döper rakt vinkel 
+
+// namn för avstånd mättning
+int Near = 0;
+int NotSafe = 350;
+int Safe = 500;
+
+int NearTurn = 15;
+int MiddleSafeTurn = 300;
+int NotSafeTurn = 200;
+int SafeTurn = 700;
+
+
+OPT3101 sensor;   // döper sensorn
+int16_t distances[3]; // aktiverar tre punkter på sensor och döper de till distances
+volatile bool dataReady = false;
+void setDataReadyFlag()
 {
-
-  
-  // Attach the Servo variable to a pin:
-    pinMode (IN1, OUTPUT);
-    pinMode (IN2, OUTPUT);
-    pinMode (ENA, OUTPUT);
- 
-
-
-   myservo.attach(servoPin);
-   myservo.write(90);
-   Wire.begin();
-   Serial.begin(9600);
-  
-  // Wait for the serial port to be opened before printing
-  // messages (only applies to boards with native USB).
-  
-  while (!Serial) {}
-
-  sensor.init();
-  if (sensor.getLastError())
-  {
-    Serial.print(F("Failed to initialize OPT3101: error "));
-    Serial.println(sensor.getLastError());
-    while (1) {}
-  }
-  sensor.setContinuousMode();
-  sensor.enableDataReadyOutput(1);
-  sensor.setFrameTiming(32);
-  sensor.setChannel(OPT3101ChannelAutoSwitch);
-  sensor.setBrightness(OPT3101Brightness::Adaptive);
-
-  attachInterrupt(digitalPinToInterrupt(dataReadyPin), setDataReadyFlag, RISING);
-  sensor.enableTimingGenerator();
-  
-}
-
-void loop() {
-
-// Döper om sensorer
-leftsensor = distances[0]; 
-middlesensor = distances[1];
-rightsensor = distances[2];
-
-
- //control speed 
-  analogWrite(ENA, Speed); 
-  
- if (dataReady)
-  {
-    sensor.readOutputRegs();
-    distances[sensor.channelUsed] = sensor.distanceMillimeters;
-      
-       if (middlesensor<Near) // Bilen backar om den är nära vägen 
-       {
-        Speed = BackSpeed;
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, HIGH);
-        
-         
-       }
-      else if (middlesensor>Near && middlesensor<NotSafe) // Bilen kör framåt men långsamt om den närmar sig väggen
-       {
-        Speed = map(middlesensor, Near, NotSafe, LowSpeed, HighSpeed);
-        digitalWrite(IN1, HIGH);
-        digitalWrite(IN2, LOW);
-       }
-       else // Bilen kör framåt
-       {
-        Speed = HighSpeed;
-         digitalWrite(IN1, HIGH);
-        digitalWrite(IN2, LOW);
-       }
-
-  
-   if (sensor.channelUsed == 2)
-    {
-      if (Speed == BackSpeed) //back svängning
-  
-  {
-    if (rightsensor<Near)
-       {
-          angle = map(rightsensor, NotSafe, Near, Straight, TurnLeft);
-          myservo.write(angle); 
-       }
-       else if (leftsensor<Near) 
-      {   
-          angle = map(leftsensor, NotSafe, Near,  Straight, TurnRight);
-          myservo.write(angle); 
-      }
-        
-  }
-
-      else if (Speed > BackSpeed) // Bilen kör framåt ochsvänger rätt
-      {
-       if (leftsensor<Safe)
-       {
-          angle = map(leftsensor, Safe, Near,  Straight, TurnRight);
-          myservo.write(angle); 
-          
-       }
-        }
-       else if (rightsensor<Safe)
-       {
-          angle = map(rightsensor, Safe, Near,  Straight, TurnLeft);
-          myservo.write(angle); 
-         
-       }       
-       else myservo.write(Straight);
-       
-  // Skriver ut sensor data:
-  
-        Serial.print("\n");
-        Serial.print("left sensor= ");
-        Serial.print(leftsensor);
-        Serial.print("\n");
-  
-        Serial.print("Middle Sensor= ");
-        Serial.print(middlesensor);
-        Serial.print("\n");
-   
-        Serial.print("Right Sensor= ");
-        Serial.print(rightsensor);
-       Serial.print("\n");
-   
-      Serial.print("Speed= ");
-        Serial.print(Speed);
-       Serial.print("\n");
-      Serial.println();
-  
-    dataReady = false;
-  }
-  }
- 
-
- 
- 
-
+  dataReady = true;
 }
