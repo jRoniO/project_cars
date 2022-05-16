bfa457fa714bac6a1daf206d0e9525a26c5f1a04
fakhreddin98 <99963603+fakhreddin98@users.noreply.github.com>
2022-05-16 19:59:55 +0200

    Update header.h

--- Fakhreddin/header.h
+++ Fakhreddin/header.h
@@ -10,40 +10,25 @@ Servo myservo;
 // Define the servo pin:
 #define servoPin 5
 
-const int IN1 = 7; // Pin nummer för motor 
-const int IN2 = 4; // Pin nummer för motor 
-const int ENA = 3; // Pin nummer för motors riktning
-
 const uint8_t dataReadyPin = 2; // Pin nummer för sensor läsning
 
 int angle = 90; // Döper svängnings vinkel till angle
-int Speed = 100; // Döper hasigheten till Speed
 
 int leftsensor = 0;
 int middlesensor = 1;
 int rightsensor = 2;
 
-// Namn för olika hasigheter för lättare ändring av hastigheten.
-int BackSpeed = 160;
-int LowSpeed = 170;
-int MiddleSpeed = 205;
-int HighSpeed = 255;
 
 // Namn för olika sväng vinklar för lättare ändring av svängvinkel
-int TurnRight = 135; // Döper HögerSvängnings vinkel
-int TurnLeft = 45; // Döper VänsterSvängnings vinkel 
-int Straight = 90; // Döper rakt vinkel 
+int Straight = 80; // Döper rakt vinkel 
+int TurnAngle = 40;
+int TurnRight = Straight - TurnAngle; // Döper HögerSvängnings vinkel
+int TurnLeft = Straight + TurnAngle; // Döper VänsterSvängnings vinkel 
 
 // namn för avstånd mättning
-int Near = 0;
+int Near = 200;
 int NotSafe = 350;
-int Safe = 500;
-
-int NearTurn = 15;
-int MiddleSafeTurn = 300;
-int NotSafeTurn = 200;
-int SafeTurn = 700;
-
+int Safe = 600;
 
 OPT3101 sensor;   // döper sensorn
 int16_t distances[3]; // aktiverar tre punkter på sensor och döper de till distances
